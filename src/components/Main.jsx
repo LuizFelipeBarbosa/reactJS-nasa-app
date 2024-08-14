bffc1b78b4044a12a48f078d07a7145cc526c3fa
Luiz Felipe Barbosa <48069633+LuizFelipeBarbosa@users.noreply.github.com>
2024-08-14 20:19:03 -0300

    fixed uncentered background on mobile

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,12 @@ function Main(props) {
 	const { img, title } = props;
 
 	return (
-		<div className="w-full h-full">
-			<img className="object-cover w-full h-full" src={img} alt={title} />
+		<div className="w-full h-full flex items-center justify-center">
+			<img
+				className="object-cover object-center w-full h-full"
+				src={img}
+				alt={title}
+			/>
 		</div>
 	);
 }
